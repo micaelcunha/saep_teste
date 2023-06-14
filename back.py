0f2e1b063c29996f0a6998617a19eb8127d901e6
Micael Cunha <micaaelvitoor2015@gmail.com>
2023-06-13 23:41:02 -0300

    ajeitando a conexão

--- back.py
+++ back.py
@@ -7,28 +7,27 @@ import random
 
 def conectarBanco(area):
     try:
-        con = mysql.connector.connect (host='localhost:3307', database='saep', user='root', password= '')
-        consulta_sql = rf"SELECT `quantidade` FROM `alocacao` WHERE area = 1;"
-        cursor = con.cursor()
-        cursor.execute(consulta_sql) 
-        con.commit()
-        print(consulta_sql)
+        conexao = mysql.connector.connect( host="localhost", user="root", password="", database="saep")
+        cursor = conexao.cursor()
+        consulta = "SELECT `quantidade` FROM `alocacao` WHERE area = %s;"
+        cursor.execute(consulta, (area,))
+        resultado = cursor.fetchall()  # recupera todos os resultados da consulta
     except Error as erro:
         print("Erro ao acessar tabela MysQL", erro)
     finally:
-        if(con.is_connected()):
-            con.close()
+        if(conexao.is_connected()):
+            conexao.close()
             cursor.close()
-            print("  Conexäo ao MysQL encerrada\n\n")
+            print("\nConexao ao MysQL encerrada\n\n")        
+            return resultado
 
 area = random.randint(1,10)
 print("area",area)
 
-conectarBanco(area)
+resultado = conectarBanco(area)
 
 
-vaga = True
-if vaga :
-    div = "brano"
+if len(resultado) > 0:
+    print("A vaga esta fechada")
 else:
-    div = "azul"
+    print("A vaga esta aberta")
